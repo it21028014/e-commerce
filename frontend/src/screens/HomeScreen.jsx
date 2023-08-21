78fc64c631ead0aa43ac5991252fa004d6b8018e
Sansika Kodithuwakku <it21028014@my.sliit.lk>
2023-08-21 22:38:56 +0530

    Home screen data read from backend

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,13 +1,21 @@
-import React from "react";
-import data from "../data";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 
+import { Link } from "react-router-dom";
+import axios from "axios";
 const HomeScreen = () => {
+  const [products, setProduct] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get("/api/products");
+      setProduct(result.data);
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       <h1>List Product</h1>
       <div className="products">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <div className="product" key={product.slung}>
             <Link to={`/product/${product.slung}`}>
               <img src={product.images} alt={product.name} />
